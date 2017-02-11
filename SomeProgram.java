a49f3d3dd85966b9847b758f15e48c27c2098db5
Wojciech Fracz <fracz@agh.edu.pl>
2017-02-11 10:17:59 +0100

    Skeleton of a code to manage the satellite

--- SomeProgram.java
+++ SomeProgram.java
@@ -3,4 +3,8 @@ public class SomeProgram {
         System.out.println("We are learning to use Git.");
         System.out.println("We will be Git experts, soon.");
     }
+    
+    public void manageTheSatellite() {
+        // TODO implement
+    }
 }
