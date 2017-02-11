7c331ea36b1418172bfc772dbe5dd67e7e2743cf
Wojciech Fracz <fracz@agh.edu.pl>
2017-02-11 09:41:58 +0100

    Further implemetation of our program

--- SomeProgram.java
+++ SomeProgram.java
@@ -1,5 +1,6 @@
 public class SomeProgram {
     public static void main(String[] args) {
         System.out.println("We are learning to use Git.");
+        System.out.println("We will be Git experts, soon.");
     }
 }
